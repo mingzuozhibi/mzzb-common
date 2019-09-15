3e33447d05a304fec85cb5a73ba3e143201cf7e2
fuhaiwei <fuhaiwei@126.com>
2019-09-15 09:50:26 +0800

    feat: Extract JmsMessage interface

--- src/main/java/mingzuozhibi/common/jms/JmsMessage.java
+++ src/main/java/mingzuozhibi/common/jms/JmsMessage.java
@@ -1,75 +1,25 @@
 package mingzuozhibi.common.jms;
 
-import com.google.gson.JsonObject;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+public interface JmsMessage {
 
-import java.time.Instant;
+    void info(String format, Object... args);
 
-@Slf4j
-@Component
-public class JmsMessage {
+    void success(String format, Object... args);
 
-    @Autowired
-    private JmsService jmsService;
+    void notify(String format, Object... args);
 
-    public void info(String format, Object... args) {
-        info(String.format(format, args));
-    }
+    void warning(String format, Object... args);
 
-    public void success(String format, Object... args) {
-        success(String.format(format, args));
-    }
+    void danger(String format, Object... args);
 
-    public void notify(String format, Object... args) {
-        notify(String.format(format, args));
-    }
+    void info(String message);
 
-    public void warning(String format, Object... args) {
-        warning(String.format(format, args));
-    }
+    void success(String message);
 
-    public void danger(String format, Object... args) {
-        danger(String.format(format, args));
-    }
+    void notify(String message);
 
-    public void info(String message) {
-        String msgData = buildMsgData("info", message);
-        jmsService.sendJson("module.message", msgData);
-        log.info("JMS -> {}: {}", "module.message", message);
-    }
+    void warning(String message);
 
-    public void success(String message) {
-        String msgData = buildMsgData("success", message);
-        jmsService.sendJson("module.message", msgData);
-        log.info("JMS -> {}: {}", "module.message", message);
-    }
-
-    public void notify(String message) {
-        String msgData = buildMsgData("notify", message);
-        jmsService.sendJson("module.message", msgData);
-        log.info("JMS -> {}: {}", "module.message", message);
-    }
-
-    public void warning(String message) {
-        String msgData = buildMsgData("warning", message);
-        jmsService.sendJson("module.message", msgData);
-        log.warn("JMS -> {}: {}", "module.message", message);
-    }
-
-    public void danger(String message) {
-        String msgData = buildMsgData("danger", message);
-        jmsService.sendJson("module.message", msgData);
-        log.error("JMS -> {}: {}", "module.message", message);
-    }
-
-    private String buildMsgData(String type, String text) {
-        JsonObject data = new JsonObject();
-        data.addProperty("type", type);
-        data.addProperty("text", text);
-        data.addProperty("createOn", Instant.now().toEpochMilli());
-        return jmsService.buildJson(data);
-    }
+    void danger(String message);
 
 }
