9b4ec41c8ec913daf58da648c7c1588bb899da33
fuhaiwei <fuhaiwei@126.com>
2019-09-25 02:01:52 +0800

    feat(jms): Update JmsService api

--- src/main/java/mingzuozhibi/common/jms/JmsService.java
+++ src/main/java/mingzuozhibi/common/jms/JmsService.java
@@ -18,12 +18,28 @@ public class JmsService {
     @Autowired
     private JmsTemplate template;
 
+    @Deprecated
     public void sendJson(String destination, String json, String info) {
         template.convertAndSend(destination, json);
         log.info("JMS -> {}: {}", destination, info);
     }
 
+    @Deprecated
     public void sendJson(String destination, String json) {
+        template.convertAndSend(destination, json);
+    }
+
+    public void sendJsonAndJmsLog(String destination, String json, String message) {
+        template.convertAndSend(destination, json);
+        log.info("JMS -> {}: {}", destination, message);
+    }
+
+    public void sendJsonAndJmsLog(String destination, String json) {
+        template.convertAndSend(destination, json);
+        log.info("JMS -> {}: {}", destination, json);
+    }
+
+    public void sendJsonNotLog(String destination, String json) {
         template.convertAndSend(destination, json);
     }
 
