6223f8085f1dcb4d19f3be06290328d0c222bf4d
fuhaiwei <fuhaiwei@126.com>
2019-09-25 16:45:16 +0800

    feat(security): Add user server error

--- src/main/java/mingzuozhibi/common/security/SecurityFilter.java
+++ src/main/java/mingzuozhibi/common/security/SecurityFilter.java
@@ -1,5 +1,6 @@
 package mingzuozhibi.common.security;
 
+import mingzuozhibi.common.BaseFilter;
 import mingzuozhibi.common.model.Content;
 import mingzuozhibi.common.spider.SpiderJsoup;
 import org.springframework.security.core.Authentication;
@@ -7,40 +8,29 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.util.StringUtils;
 
-import javax.servlet.*;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class SecurityFilter implements Filter {
+public class SecurityFilter extends BaseFilter {
 
     @Override
-    public void init(FilterConfig filterConfig) {
-
-    }
-
-    @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        HttpServletRequest request = (HttpServletRequest) servletRequest;
+    public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String username = request.getHeader("X-USERNAME");
         if (StringUtils.hasText(username)) {
-            checkAuthentication(username);
-        }
-        filterChain.doFilter(servletRequest, servletResponse);
-    }
-
-    @Override
-    public void destroy() {
-
-    }
-
-    private void checkAuthentication(String username) {
-        String url = "http://localhost:9999/userDetails/" + username;
-        Content content = Content.parse(SpiderJsoup.waitRequest(url));
-        if (content.isSuccess()) {
+            String url = "http://localhost:9999/userDetails/" + username;
+            Content content = Content.parse(SpiderJsoup.waitRequest(url));
+            if (!content.isSuccess()) {
+                responseError(response, "用户服务不可用");
+                return;
+            }
             UserDetails userDetails = content.parseData(SimpleUserDetails.class);
             Authentication authentication = new SimpleAuthentication(userDetails);
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+        chain.doFilter(request, response);
     }
 
 }
