3d0c8614eda0a2c73669240c7d65f219b64571f9
fuhaiwei <fuhaiwei@126.com>
2019-10-05 12:11:36 +0800

    feat(security): Add user details cache

--- src/main/java/mingzuozhibi/common/security/SecurityFilter.java
+++ src/main/java/mingzuozhibi/common/security/SecurityFilter.java
@@ -13,24 +13,37 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SecurityFilter extends BaseFilter {
+
+    private static Map<String, UserDetails> cached = new HashMap<>();
 
     @Override
     public void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String username = request.getHeader("X-USERNAME");
         if (StringUtils.hasText(username)) {
-            String url = "http://localhost:9999/userDetails/" + username;
-            Content content = Content.parse(SpiderJsoup.waitRequest(url));
-            if (!content.isSuccess()) {
-                responseError(response, "用户服务不可用");
-                return;
+            if (!cached.containsKey(username)) {
+                String url = "http://localhost:9999/userDetails/" + username;
+                Content content = Content.parse(SpiderJsoup.waitRequest(url));
+                if (!content.isSuccess()) {
+                    responseError(response, "用户服务不可用");
+                    return;
+                }
+                cached.put(username, content.parseData(SimpleUserDetails.class));
             }
-            UserDetails userDetails = content.parseData(SimpleUserDetails.class);
+            UserDetails userDetails = cached.get(username);
             Authentication authentication = new SimpleAuthentication(userDetails);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+        } else {
+            SecurityContextHolder.createEmptyContext();
         }
-        chain.doFilter(request, response);
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            SecurityContextHolder.clearContext();
+        }
     }
 
 }
